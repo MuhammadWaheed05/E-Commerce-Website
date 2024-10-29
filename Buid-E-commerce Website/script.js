a4cff436c6c6eb667cdc18f6ab5430994531bd8d
Muhammad Waheed <muhammadwaheed0.llc@gmail.com>
2024-10-29 15:55:13 +0500

    Add files via upload

---
+++ Buid-E-commerce Website/script.js
@@ -0,0 +1,19 @@
+let mainImg = document.getElementById("mainimg");
+let smallImg = document.getElementById("smallimg");
+
+smallImg[0].onclick = function()
+{
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function()
+{
+    mainimg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function()
+{
+    mainimg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function()
+{
+    mainImg.src = smallImg[3].src;
+}
